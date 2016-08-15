692baf5048706f98f7e67d5d101aa62103dc1243
ILya-Lev <lis2889@gmail.com>
2016-08-16 00:27:13 +0300

    Added class Wallet

---
+++ DomainModel/Wallet.cs
@@ -0,0 +1,14 @@
+﻿namespace DomainModel
+{
+	public class Wallet
+	{
+		/// <summary> money available for spending this time period, i.e. month </summary>
+		public decimal Balance { get; set; }
+
+		/// <summary> savings in UAH </summary>
+		public decimal SavingsGrivna { get; set; }
+
+		/// <summary> savings in USD </summary>
+		public decimal SavingsDollar { get; set; }
+	}
+}
